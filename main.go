74e142599f979464abb54aedc5619087cbf6a0aa
ycj <yingchaojie@163.com>
2023-11-30 16:53:44 +0800

    fix

--- main.go
+++ main.go
@@ -5,14 +5,15 @@ import (
 	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/chromedp"
 	"log"
-	"time"
 )
 
 func main() {
-	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.Flag("headless", false), // 开启图像界面
+	// 取消headless
+	opts := append(
+		chromedp.DefaultExecAllocatorOptions[3:],
+		chromedp.NoFirstRun,
+		chromedp.NoDefaultBrowserCheck,
 		//chromedp.ProxyServer("http://10.10.1.1:21869"), // 设置代理访问
-		chromedp.Flag("mute-audio", false), // 关闭声音
 	)
 	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
 	defer cancel()
@@ -21,8 +22,8 @@ func main() {
 	defer cancel()
 
 	// create a timeout
-	ctx, cancel = context.WithTimeout(ctx, 15*time.Second)
-	defer cancel()
+	//ctx, cancel = context.WithTimeout(ctx, 15*time.Second)
+	//defer cancel()
 
 	// navigate to a page, wait for an element, click
 	var example string
@@ -30,17 +31,24 @@ func main() {
 	err := chromedp.Run(ctx,
 		chromedp.Navigate(`https://www.baidu.com`),
 		// wait for footer element is visible (ie, page is loaded)
-		chromedp.WaitVisible(`body > #wrapper`),
+		chromedp.WaitVisible(`#kw`, chromedp.ByID),
 		// find and click "Example" link
 		//chromedp.Click(`#example-After`, chromedp.NodeVisible),
 		// retrieve the text of the textarea
-		chromedp.Nodes(`#s-top-left`, &nodes),
+		//chromedp.Nodes(`#s-top-left`, &nodes),
+		chromedp.InnerHTML("#s-top-left", &example),
+		//chromedp.Sleep(time.Duration(3)*time.Second),
+		chromedp.SendKeys("#kw", "test", chromedp.ByID),
+		//chromedp.Click("#su"),
+		chromedp.Submit("#form", chromedp.ByID),
 	)
 	if err != nil {
 		log.Fatal("error: ", err)
 	}
 	for _, e := range nodes {
-		log.Println(e.Name, e.Value)
+		log.Println(len(e.Children))
+		log.Println(e.Attributes)
 	}
 	log.Println(example)
+	select {}
 }
